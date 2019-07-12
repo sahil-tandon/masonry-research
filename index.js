d533b525ab297087686f345b61752e3ed0c04ff1
Sahil Tandon <sahil.tandon@live.com>
2019-07-12 02:46:00 -0400

    Reinitialize isotope after tile open.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-$('.grid').isotope({
+const isotopeOptions = {
   itemSelector: '.grid-item',
 	masonry : {
 		columnWidth: 220,
 		gutter: 10,
     }
-});
+};
+
+$('.grid').isotope(isotopeOptions);
+
 $('.grid-item').on('click', function(){
   $(this).toggleClass("grid-item__open");
+  $('.grid').isotope(isotopeOptions);
 });
