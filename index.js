8ab960f3eb0edf2eaae39990e184e9d2b7056948
Sahil Tandon <sahil.tandon@live.com>
2019-07-12 19:14:10 -0400

    Updates to JS

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const isotopeOptions = {
-  itemSelector: '.grid-item',
+  itemSelector: '.grid-item',  
 	masonry : {
 		columnWidth: 220,
-		gutter: 10,
-    }
+    gutter: 10,    
+  },
+  stagger: 30
 };
 
 $('.grid').isotope(isotopeOptions);
@@ -12,3 +13,8 @@ $('.grid-item').on('click', function(){
   $(this).toggleClass("grid-item__open");
   $('.grid').isotope(isotopeOptions);
 });
+
+$('.grid-filters li a').click(function(e){
+  e.preventDefault();
+  $('.grid').isotope({ filter: $(this).parent('li').data('target') });
+})
